c78092bc1548a238292f91b92476415a3a59370b
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-07 16:24:54 +0700

    Update Home.js
    Signed-off-by: DinhHoangGia-pro <dinhhoanggia@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -8,19 +8,40 @@ const Home=()=>
 
      const Themsv=()=>
         {
+            console.log("Test them sinh vien");
             const sinhvienmoi={id:5,hoten:"Cherry Nguyen", lop:"221406" };
             array2=[...array2, sinhvienmoi];
     
         }
 
 
+        const Themsv2=()=>
+            {
+                console.log("Test them sinh vien");
+                const sinhvienmoi={id:5,hoten:"Cherry Nguyen", lop:"221406" };
+                const newarray3=[...array3, sinhvienmoi];
+                //array3=newarray3;
+                setArray3(newarray3);
+
+        
+            }
+
+
      var array2=[{id:1,hoten:"Nguyễn Văn A", lop:"221406" },
                         {id:2,hoten:"Nguyễn Văn Bình", lop:"221406" },
                         {id:3,hoten:"Nguyễn Văn Cường", lop:"221406" },
                         {id:4,hoten:"Trần Thị D", lop:"221406" }];
-        Themsv();
+        //Themsv();
 
-    
+    const [array3,setArray3]=useState([
+        {id:1,hoten:"Nguyễn Văn A", lop:"221406" },
+                        {id:2,hoten:"Nguyễn Văn Bình", lop:"221406" },
+                        {id:3,hoten:"Nguyễn Văn Cường", lop:"221406" },
+                        {id:4,hoten:"Trần Thị D", lop:"221406" }
+
+    ]
+
+    );
 
     
        
@@ -58,7 +79,28 @@ const Home=()=>
                 )
              }  
 
-             <p>________________________</p>         
+             <p>________________________</p>   
+
+             <p><input type="button" value="Them sinh vien" onClick={Themsv.bind(this) } />   </p>      
+
+
+             {
+                array3.map(x=>{
+                    return(
+                        <p>
+                            <p>Ma sinh vien: {x.id}</p>
+                            <p>Ten sinh vien: {x.hoten}</p>
+                            <p>Lop: {x.lop}</p>
+                        </p>
+                    )
+                }
+
+                )
+             }  
+
+             <p>________________________</p> 
+             <p><input type="button" style={{backgroundColor:"blue"}  } value="Them sinh vien" onClick={Themsv2.bind(this) } />   </p>
+
 
 
             
